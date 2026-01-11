b009afc1cb31c029644f83256c7c80e27e4b8f64
Samir Tanweer <samirtanweer11@gmail.com>
2026-01-11 23:45:50 +0530

    Add SQL query files

---
+++ 00_Project_Overview.sql
@@ -0,0 +1,22 @@
+-- PROJECT: SQL Pizza Sales Analysis
+-- Author: Samir Tanweer
+-- Tools: MySQL, CSV Data
+-- Dataset: Pizza Sales Dataset
+
+-- Questions solved in this project:
+
+-- Q1: Total number of orders placed
+-- Q2: Total revenue generated
+-- Q3: Highest priced pizza
+-- Q4: Most common pizza size ordered
+-- Q5: Top 5 most ordered pizza types with quantity
+
+-- Q6: Total quantity ordered per pizza category
+-- Q7: Distribution of orders by hour
+-- Q8: Category-wise distribution of pizzas
+-- Q9: Average pizzas ordered per day
+-- Q10: Top 3 pizzas by revenue
+
+-- Q11: Percentage contribution of each pizza to total revenue
+-- Q12: Cumulative revenue over time
+-- Q13: Top 3 pizzas by revenue per category
